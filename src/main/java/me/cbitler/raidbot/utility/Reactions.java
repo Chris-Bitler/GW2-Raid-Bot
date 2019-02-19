28d6960374eb1513c089814b11628ce7f835f50d
Jeremy D. Thralls <thralls@gmx.net>
2019-02-19 20:04:10 +0100

    swap ids for those from the test server

--- src/main/java/me/cbitler/raidbot/utility/Reactions.java
+++ src/main/java/me/cbitler/raidbot/utility/Reactions.java
@@ -12,46 +12,46 @@ public class Reactions {
      * List of reactions representing classes
      */
     static String[] specs = {
-            "Dragonhunter", //387295988282556417
-            "Firebrand", //387296167958151169
-            "Herald", //387296053659172869
-            "Renegade", //387296192381321226
-            "Berserker", //387296013947502592
-            "Spellbreaker", //387296212421836800
-            "Scrapper", //387296081823662081
-            "Holosmith", //387296176770121738
-            "Druid", // 387296044716916738
-            "Soulbeast", //387296205488521216
-            "Daredevil", //387296029533274113
-            "Deadeye", //387296159716081664
-            "Weaver", //387296219988361218
-            "Tempest", //387296089340117002
-            "Chronomancer", //387296021710897152
-            "Mirage", //387296184114610185
-            "Reaper", //387296061997318146
-            "Scourge" //387296198928891905
+            "Dragonhunter", //547490713764364329
+            "Firebrand", //547490713928204308
+            "Herald", //547490714049708032
+            "Renegade", //547490713584140323
+            "Berserker", //547490714041450496
+            "Spellbreaker", //547490713990987806
+            "Scrapper", //547490714079068184
+            "Holosmith", //547490713911296040
+            "Druid", // 547490713894518792
+            "Soulbeast", //547490714041319439
+            "Daredevil", //547490713927942169
+            "Deadeye", //547490714209222676
+            "Weaver", //547490714007764994
+            "Tempest", //547490714045644810
+            "Chronomancer", //547490713567232021
+            "Mirage", //547490714624458752
+            "Reaper", //547490713642729478
+            "Scourge" //547490713965690897
     };
 
     static Emote[] reactions = {
-            getEmoji("387295988282556417"), // Dragonhunter
-            getEmoji("387296167958151169"), // Firebrand
-            getEmoji("387296053659172869"), // Herald
-            getEmoji("387296192381321226"), // Renegade
-            getEmoji("387296013947502592"), // Berserker
-            getEmoji("387296212421836800"), // Spellbreaker
-            getEmoji("387296081823662081"), // Scrapper
-            getEmoji("387296176770121738"), // Holosmith
-            getEmoji("387296044716916738"), // Druid
-            getEmoji("387296205488521216"), // Soulbeast
-            getEmoji("387296029533274113"), // Daredevil
-            getEmoji("387296159716081664"), // Deadeye
-            getEmoji("387296219988361218"), // Weaver
-            getEmoji("387296089340117002"), // Tempest
-            getEmoji("387296021710897152"), // Chronomancer
-            getEmoji("387296184114610185"), // Mirage
-            getEmoji("387296061997318146"), // Reaper
-            getEmoji("387296198928891905"), // Scourge
-            getEmoji("387346852867211274") // X_
+            getEmoji("547490713764364329"), // Dragonhunter
+            getEmoji("547490713928204308"), // Firebrand
+            getEmoji("547490714049708032"), // Herald
+            getEmoji("547490713584140323"), // Renegade
+            getEmoji("547490714041450496"), // Berserker
+            getEmoji("547490713990987806"), // Spellbreaker
+            getEmoji("547490714079068184"), // Scrapper
+            getEmoji("547490713911296040"), // Holosmith
+            getEmoji("547490713894518792"), // Druid
+            getEmoji("547490714041319439"), // Soulbeast
+            getEmoji("547490713927942169"), // Daredevil
+            getEmoji("547490714209222676"), // Deadeye
+            getEmoji("547490714007764994"), // Weaver
+            getEmoji("547490714045644810"), // Tempest
+            getEmoji("547490713567232021"), // Chronomancer
+            getEmoji("547490714624458752"), // Mirage
+            getEmoji("547490713642729478"), // Reaper
+            getEmoji("547490713965690897"), // Scourge
+            getEmoji("547490713818890266") // X_
     };
 
     /**
